05ecf0f5084753e84ef17c88107497b650767ba8
takewiki <hulilei@takewiki.com.cn>
2019-11-16 11:58:53 +0800

    add the demo2

---
+++ R/myfunc.R
@@ -0,0 +1,15 @@
+#' 加法算法
+#'
+#' @param x 第一个参数
+#' @param y 第二个参数
+#'
+#' @return 返回加法结果
+#' @export
+#'
+#' @examples
+#' bb <- add(2,3);
+add <- function(x,y) {
+  res <-x+y
+  return(res)
+
+}
---
+++ data-raw/TEST.R
@@ -0,0 +1,10 @@
+library(demoR2);
+
+
+
+bb <- add(2,3);
+bb;
+
+
+? add
+bb <- add(2,3);
